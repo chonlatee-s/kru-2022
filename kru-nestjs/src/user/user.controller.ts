df65b0b7d41ab393047394d6030bf136ce7c24a8
Chonlatee <kruchonlatee@gmail.com>
2021-09-25 21:34:28 +0700

    done findAll post user

--- kru-nestjs/src/user/user.controller.ts
+++ kru-nestjs/src/user/user.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Post, Query } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { User } from './interfaces/user.interface';
 import { UserService } from './user.service';
